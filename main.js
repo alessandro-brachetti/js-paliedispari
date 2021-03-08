3e8e85c40688a10a2c991a7f7ae52c5240240936
Alessandro Brachetti <alebrack13@gmail.com>
2021-03-08 18:27:35 +0100

    Esercizi completi

--- main.js
+++ main.js
@@ -1,35 +1,25 @@
 //PALINDROMA
 //Chiedere all’utente di inserire una parola
-//Creare una funzione per capire se la parola inserita è palindroma
-
   var parola = prompt("Scrivi una parola");
   var parolaLower = parola.toLowerCase();
 
-  palindroma(parola);
-  
-//FUNCTIONS
-function palindroma (parola) {
-  var parolaReverse = parolaLower.split('').reverse().join('');
+  console.log(parolaLower);
+//Creare una funzione per capire se la parola inserita è palindroma
+  var check = palindroma(parola);
 
-  return parolaReverse === parolaLower;
+//FUNCTIONS
+function palindroma(parola) {
+  var parolaReverse = parolaLower.split('').reverse().join('');
+  var flag = false;
+  if (parolaReverse === parolaLower) {
+    flag = true;
+    console.log("La parola è palindroma!");
+  } else {
+    console.log("La parola non è palindroma!");
+  }
+  return flag;
 }
 
-// var utente = prompt("Scrivi una parola");
-//
-// palindrome(utente);
-//
-// function palindrome(parola) {
-//  var parola = utente.toLowerCase();
-//  var len = parola.length;
-//  for (var i = 0; i < len/2; i++) {
-//    if (parola[i] !== parola[len - 1 - i]) {
-//        return false;
-//        console.log("NO");
-//    }
-//  }
-//  return true;
-//  console.log("SI");
-// }
 
 //PARI E DISPARI
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -38,19 +28,18 @@ console.log(utente);
 var utenteNum = parseInt(prompt("Dammi un numero da 1 a 5"));
 console.log(utenteNum);
 
-var pcNum ;
-randomNum(1, 5);//Generiamo un numero random sempre da 1 a 5
-
+var pcNum = randomNum(1, 5);//Generiamo un numero random sempre da 1 a 5
 console.log(pcNum);
-
 
 var somma = somma(utenteNum, pcNum);//Sommiamo i 2 numeri
 console.log(somma);
 
-if (somma % 2 == 0) { //Dichiariamo chi ha vinto.
-  console.log("Il pari vince");
-} else {
-  console.log("Il dispari vince");
+if (somma % 2 == 0 && utente=="pari") { //Dichiariamo chi ha vinto.
+  console.log("L'utente vince!");
+} else if (somma % 2 !== 0 && utente=="dispari"){
+  console.log("L'utente vince!");
+} else  {
+  console.log("Il PC vince!");
 }
 
 //FUNCTIONS
